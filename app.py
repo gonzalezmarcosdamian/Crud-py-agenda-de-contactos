ce759c112095fb62ea06d3bde9072131233358ea
Marcos damian Gonzalez <ingonzalezdamian@gmail.com>
2021-01-10 16:07:16 -0300

    Agregar contacto Finalizada con validacion

--- app.py
+++ app.py
@@ -2,6 +2,16 @@
 import os
 
 CARPETA = 'contactos/'  # carpeta de contactos
+EXTENSION = '.txt'  # Extension de archivo
+
+# Contactos
+
+
+class Contacto:
+    def __init__(self, nombre, telefono, categoria):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.categoria = categoria
 
 
 def app():
@@ -20,7 +30,7 @@ def app():
 
         # Ejecutar las opciones, no existe switch py
         if opcion == 1:
-            print('Agregar contacto')
+            agregar_contacto()
             preguntar = False
         elif opcion == 2:
             print('Editar contacto')
@@ -36,7 +46,35 @@ def app():
             preguntar = False
         else:
             print('Opcion no valida')
+
 # Funciones
+
+
+def agregar_contacto():
+    print('Escribe los datos para agregar el nuevo Contacto:')
+    nombre_contacto = input('Nombre del contacto:\r\n')
+
+    # Revisar si existe el archivo
+    existe = os.path.isfile(CARPETA+nombre_contacto+EXTENSION)
+    if not existe:
+        with open(CARPETA+nombre_contacto+EXTENSION, 'w') as archivo:
+            telefono_contacto = input('Agrega el telefono: \r\n')
+            categoria_contacto = input('Categoria contacto: \r\n')
+
+            # instanciamos la claes
+            contacto = Contacto(
+                nombre_contacto, telefono_contacto, categoria_contacto)
+
+            archivo.write('Nombre: '+contacto.nombre+'\r\n')
+            archivo.write('Telefono: '+contacto.telefono+'\r\n')
+            archivo.write('Categoria: '+contacto.categoria+'\r\n')
+
+            print('\r\n Contacto creado correctamente! \r\n')
+    else:
+        print('Ese contacto ya existe!\r\n')
+
+    # Reiniciar la app
+    app()
 
 
 def mostrar_menu():
